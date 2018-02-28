6909e414ea2777ac28e96c4e9ce91f25a502c1ab
Stephen Harrington <stephenharrington04@yahoo.com>
2018-02-28 22:08:53 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("fantasy", 1, 2);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("fiction", 2, 1);
+
+INSERT INTO books (title, year, series_id) VALUES ("GOT", 2000, 1);
+INSERT INTO books (title, year, series_id) VALUES ("GOT", 2000, 1);
+INSERT INTO books (title, year, series_id) VALUES ("GOT", 2000, 1);
+INSERT INTO books (title, year, series_id) VALUES ("GOT", 2000, 1);
+INSERT INTO books (title, year, series_id) VALUES ("GOT", 2000, 1);
+INSERT INTO books (title, year, series_id) VALUES ("GOT", 2000, 1);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Sam", "I'm craven", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Jon", "I'm craven", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Edgar", "I'm craven", "Human", 1, 3);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Jamie", "I'm craven", "Human", 1, 4);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Cerci", "I'm craven", "Human", 1, 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Danereas", "I'm craven", "Human", 1, 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Imp", "I'm craven", "Human", 1, 5);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Benjen", "I'm craven", "Human", 1, 6);
+
+INSERT INTO subgenres (name) VALUES ("sci-fi");
+INSERT INTO subgenres (name) VALUES ("myths");
+
+INSERT INTO authors (name) VALUES ("George R. R. Martin");
+INSERT INTO authors (name) VALUES ("Tolkien");
+
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
